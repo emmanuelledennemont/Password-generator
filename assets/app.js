61e6ea0fe6a9809319e7814dbd99709e99d31d9a
Emmanuelle  Dennemont <emmanuelledennemont@macbook-pro-de-emmanuelle.home>
2022-11-26 11:44:50 +0100

    composer php version

--- assets/app.js
+++ assets/app.js
@@ -6,19 +6,46 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.scss';
-
+import "./styles/app.scss";
 
 // start the Stimulus application
-import './bootstrap';
+import "./bootstrap";
 
-const btnCopy = document.querySelector('.btn-copy'); 
-const passwordText = document.querySelector('.password-text');
+const btnCopy = document.querySelector(".btn-copy");
+const passwordText = document.querySelector(".password-text");
+
+btnCopy.addEventListener("click", () => {
+  navigator.clipboard.writeText(passwordText.innerText);
+  btnCopy.innerText = "Copied !";
+});
 
 
-btnCopy.addEventListener('click', () => {
+//   const form = document.getElementById("js-form");
 
-    navigator.clipboard.writeText(passwordText.innerText);
-    btnCopy.innerText= "Copied !";
-  
-})
+//   const passwordPreferences = localStorage.getItem("password-preferences");
+
+//   if (passwordPreferences) {
+//     lengthSelect.value = passwordPreferences.length;
+//     uppercaseChekcbox.checked = passwordPreferences.uppercase;
+//     digitsCheckbox.checked = passwordPreferences.digits;
+//     specialCharactersCheckbox.checked = passwordPreferences.special;
+//   }
+
+//   const lengthSelect = document.getElementById("length");
+//   const uppercaseChekcbox = document.getElementById("uppercase");
+//   const digitsCheckbox = document.getElementById("digits");
+//   const specialCharactersCheckbox = document.getElementById("special");
+
+//   form.addEventListener("submit", (e) => {
+//       e.preventDefault();
+//     localStorage.setItem(
+//       "password-preferences",
+//       JSON.stringify({
+//         length: parseInt(lengthSelect.value, 10),
+//         uppercase: uppercaseChekcbox.value,
+//         digits: digitsCheckbox.checked,
+//         special: specialCharactersCheckbox.checked,
+//       })
+//     );
+//   });
+
--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -4,7 +4,6 @@ namespace App\Controller;
 
 use App\Service\PasswordGenerator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
